74bc6324decb16f40071f512b2dbf3e642e375bf
vbaker007 <vbaker007@hotmail.com>
2015-10-07 22:50:01 -0600

    setup model associations

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,2 +1,3 @@
 class Scenario < ActiveRecord::Base
+  belongs_to :chapter
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,4 @@
 class Story < ActiveRecord::Base
+  has_many :chapters
+  has_many :scenarios, through :chapters
 end
