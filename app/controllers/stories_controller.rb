7b04f933ab240771b448473bf5634c3c42405927
vbaker007 <vbaker007@hotmail.com>
2015-10-07 21:19:43 -0600

    CRUD actions for Stories Controller. Note: must improve upon CRUD actions

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,48 @@
 class StoriesController < ApplicationController
+  before_action :find_story, only: [:show, :edit, :update, :destroy]
   def index
+    @stories = Story.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @story = Story.new
   end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to @story
+    else 
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to @story
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @story.destroy
+    redirect_to root_path
+  end
+
+  private
+    def find_story
+      @story = Story.find(params[:id])
+    end
+
+    def story_params
+      params.require(:story).permit(:title, :desc, :base_text, :image)
+    end
+
 end
