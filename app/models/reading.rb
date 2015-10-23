ed21d4fa0a4c60e607c39716a4fb59e82256a26b
vbaker007 <vbaker007@hotmail.com>
2015-10-23 03:59:53 -0400

    added devise sign_up link, sign_in link, & sign_out page to welcome.html.erb file

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,3 +1,21 @@
 class Reading < ActiveRecord::Base
   belongs_to :user
+
+  def update(story_id, chapter_id, scenario_id, complete)
+    self.readings.each do |reading|
+      reading.story_id = story_id
+      reading.chapter_id = chapter_id
+      reading.scenario_id = scenario_id
+      reading.complete = complete
+      reading.save 
+    end
+  end
+
+  #def select_scenario(scenario_id, complete)
+    #if 
+
+
+
+
+
 end
