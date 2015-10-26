b801d77f3cea6933e0540f9489e43b4c0b059392
vbaker007 <vbaker007@hotmail.com>
2015-10-26 14:07:56 -0400

    updated scenario model code

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,3 +1,15 @@
 class Scenario < ActiveRecord::Base
   belongs_to :chapter
+
+  def initialize(scenario_id, content)
+    @scenario_id = scenario_id
+    @content = content
+  end
+
+  def create_scenario
+     @scenario_id
+     @content
+  end
+
+
 end
