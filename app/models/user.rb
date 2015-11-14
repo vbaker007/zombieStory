dc11bb31d415bb1d7a22e57eaa6436175b32ddbc
vbaker007 <vbaker007@hotmail.com>
2015-11-14 17:39:38 -0500

    changed URL in models/user.rb & models/story.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" },
     :default_url => "/images/:style/missing.png",
-    :url  => ":http://darkshelters.herokuapp.com/",
+    :url  => ":s3_domain_url",
     :path => "public/avatars/:id/:style_:basename.:extension",
     :storage => :fog,
     :fog_credentials => {
